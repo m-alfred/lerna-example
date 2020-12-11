5c865f6348409649c1af91ce79671ca546a1aa7d
miaokefu <269436241@qq.com>
2020-12-11 15:59:16 +0800

    feat: update module b

--- packages/module-b/lib/module-b.js
+++ packages/module-b/lib/module-b.js
@@ -5,3 +5,12 @@ module.exports = moduleB;
 function moduleB() {
     return 'it\'s module b';
 }
+
+
+const _ = require('lodash');
+
+function testLodash() {
+    return JSON.stringify(_.assign({a: 1}, {name: 'alfred'}));
+}
+const data = testLodash();
+console.log('data: ' + data + ' in module b');
