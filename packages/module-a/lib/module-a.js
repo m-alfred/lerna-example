b2c2dbb6f1f8a168dcaaedf1f3bc262cde46e633
miaokefu <269436241@qq.com>
2020-12-13 12:40:56 +0800

    feat: module-a 0.0.5

--- packages/module-a/lib/module-a.js
+++ packages/module-a/lib/module-a.js
@@ -1,7 +1,7 @@
 const moduleB = require('@m_alfred/module-b');
 
 function moduleA() {
-  return 'it\'s module a';
+  return 'it\'s module a version: 0.0.5';
 }
 
 console.log('moduleB:', moduleB());
